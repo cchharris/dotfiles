61fee438aab288b20226cc29131a059836c03044
Christopher Harris <cchharris@outlook.com>
2025-02-18 16:55:26 -0800

    Add config

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -1,3 +1,4 @@
+-- See https://github.com/williamboman/mason-lspconfig.nvim
 return {
 	"williamboman/mason.nvim",
 	 dependencies = { "williamboman/mason-lspconfig.nvim", "WhoIsSethDaniel/mason-tool-installer.nvim" },
@@ -5,24 +6,29 @@ return {
 		require("mason").setup({ })
 		require("mason-lspconfig").setup({
 		ensure_installed = {
-			"bashls",
-			"buf_ls",
-			"clangd",
-			"cmake",
-			"dockerls",
-			"html",
-			"ts_ls",
-			"lua_ls",
-			"nil_ls",
-			"pyright",
-			"ruff",
-			"rust_analyzer",
-			"bzl",
-			"terraformls",
-			"tflint",
-			"harper_ls",
-			"vimls",
-			"bashls"
+			"bashls", -- bash
+			"buf_ls", -- protobuf
+			"clangd", -- c, c++
+			"cmake", -- cmake
+			"dockerls", -- docker
+			"html", -- html
+			"ts_ls", -- typescript, javascript
+			"lua_ls", -- lua
+			"nil_ls", -- nix
+			"pyright", -- python
+			"ruff", -- pytho
+			"rust_analyzer", -- rust
+			"bzl", -- bazel/skylark
+			"terraformls", -- terraform
+			"tflint", -- terraform
+			"harper_ls", -- toml, typescript, rust, ruby, python, markdown, lua, javascript, java, C, C++, C#
+			"taplo", -- toml
+			"vimls", -- vimscript
+			"yamlls", -- yaml
+			"zls", -- zig
+			"grammarly", -- text, markdown
+			"powershell_es", -- powershell
+			"jsonls", -- json
 		}
 		})
 	end
