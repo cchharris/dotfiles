9aa3baff6ba40b2a03731762c9927b383bf9eaf3
Christopher Harris <cchharris@outlook.com>
2025-03-05 11:49:37 -0800

    Add window keymaps, setup window picker

---
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,4 @@
+vim.keymap.set('n', '<leader>wh', "<C-w>h", { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>wj', "<C-w>j", { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>wk', "<C-w>k", { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>wl', "<C-w>l", { noremap = true, silent = true })
--- dot_config/nvim/lua/plugins/nvim-window-picker.lua
+++ dot_config/nvim/lua/plugins/nvim-window-picker.lua
@@ -6,4 +6,16 @@ return {
     config = function()
         require'window-picker'.setup()
     end,
+	keys = {
+		{
+			'<leader>wp',
+			function()
+				local window = require('window-picker').pick_window({hint='floating-big-letter', show_prompt=false})
+				if window then
+					vim.api.nvim_set_current_win(window)
+				end
+			end,
+			desc = "<WindowPicker> Pick window",
+		},
+	}
 }
