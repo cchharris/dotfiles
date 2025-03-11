f55b6002c871f703f258d85f7985c126d8ff0ab5
Christopher <cchharris@outlook.com>
2025-03-10 20:41:34 -0700

    Add CommandNotFound

--- readonly_OneDrive/readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_OneDrive/readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -639,3 +639,8 @@ if (Test-Path -Path $localprofile) {
 	.$localprofile
 }
 
+
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58
