f293106ca952bcb8330f7ef9c78a7ca0d16fb6f0
Christopher Harris <cchharris@outlook.com>
2025-03-13 15:32:20 -0700

    Add auto-session

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -9,3 +9,5 @@ vim.cmd [[set cursorline]]
 vim.cmd [[set tabstop=4]]
 vim.cmd [[set shiftwidth=4]]
 vim.cmd [[set clipboard+=unnamedplus]]
+
+vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
