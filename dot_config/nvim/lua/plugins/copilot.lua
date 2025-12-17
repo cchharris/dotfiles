e61b743401cd37f1cd3e9c60a7adacdff55635bd
Christopher Harris <cchharris@outlook.com>
2025-12-17 14:47:34 -0800

    Turn on copilot next edit prediction.  Add more hotkeys

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -1,21 +1,29 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-	opts = {
-		-- suggestion = { enabled = false }, -- copilot cmp does this instead
-		-- panel = { enabled = false }, -- copilot cmp does this instead
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = {
+        -- suggestion = { enabled = false }, -- copilot cmp does this instead
+        -- panel = { enabled = false }, -- copilot cmp does this instead
         panel = { enabled = false },
         suggestion = {
             enabled = false,
             auto_trigger = true,
             keymap = {
-                accept="<tab>",
-                accept_word="<c-space>",
-                accept_line="<c-l>",
-                next="<c-j>",
-                prev="<c-k>",
+                accept = "<tab>",
+                accept_word = "<c-space>",
+                accept_line = "<c-l>",
+                next = "<c-j>",
+                prev = "<c-k>",
             },
-        }
-	}
+        },
+        nes = {
+            enabled = true,
+            keymap = {
+                accept_and_goto = "<tab>",
+                accept = false,
+                dismiss = "<Esc>",
+            },
+        },
+    }
 }
--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -1,26 +1,51 @@
 return {
-"lewis6991/gitsigns.nvim",
-event = { 'BufNewFile', 'BufReadPost' },
-cmd = { 'Gitsigns' },
-opts = {
-		current_line_blame = true,
-		current_line_blame_opts = {
-			virt_text = true,
-			virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-			delay = 1000,
-		},
-		numhl=true,
-	},
-keys = {
-		{
-			"]g",
-			"<cmd>Gitsigns next_hunk<cr>",
-			desc = " Go to next git hunk"
-		},
-		{
-			"[g",
-			"<cmd>Gitsigns prev_hunk<cr>",
-			desc = " Go to previous git hunk"
-		},
-	}
+    "lewis6991/gitsigns.nvim",
+    event = { 'BufNewFile', 'BufReadPost' },
+    cmd = { 'Gitsigns' },
+    opts = {
+        current_line_blame = true,
+        current_line_blame_opts = {
+            virt_text = true,
+            virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+            delay = 1000,
+        },
+        numhl = true,
+    },
+    keys = {
+        {
+            "]g",
+            "<cmd>Gitsigns next_hunk<cr>",
+            desc = " Go to next git hunk"
+        },
+        {
+            "[g",
+            "<cmd>Gitsigns prev_hunk<cr>",
+            desc = " Go to previous git hunk"
+        },
+        {
+            "<leader>gb",
+            "<cmd>Gitsigns blame_line<cr>",
+            desc = " Open git blame"
+        },
+        {
+            "<leader>gB",
+            "<cmd>Gitsigns blame<cr>",
+            desc = " Open git blame for buffer"
+        },
+        {
+            "<leader>gd",
+            "<cmd>Gitsigns diffthis<cr>",
+            desc = " Open diff"
+        },
+        {
+            "<leader>gp",
+            "<cmd>Gitsigns preview_hunk<cr>",
+            desc = " Preview hunk"
+        },
+        {
+            "<leader>gP",
+            "<cmd>Gitsigns preview_hunk_inline<cr>",
+            desc = " Preview hunk inline"
+        },
+    }
 }
