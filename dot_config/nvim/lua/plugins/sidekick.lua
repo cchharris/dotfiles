a940a295a1a73fd13705ed524223c1a50140bdd9
Christopher Harris <cchharris@outlook.com>
2026-01-08 21:32:55 -0800

    Updates

--- dot_config/nvim/lua/plugins/sidekick.lua
+++ dot_config/nvim/lua/plugins/sidekick.lua
@@ -28,48 +28,48 @@ return {
             mode = { "n", "t", "i", "x" },
         },
         {
-            "<leader>aa",
+            "<leader>sa",
             function() require("sidekick.cli").toggle() end,
             desc = "Sidekick Toggle CLI",
         },
         {
-            "<leader>as",
+            "<leader>ss",
             function() require("sidekick.cli").select() end,
             -- Or to select only installed tools:
             -- require("sidekick.cli").select({ filter = { installed = true } })
             desc = "Select CLI",
         },
         {
-            "<leader>ad",
+            "<leader>sd",
             function() require("sidekick.cli").close() end,
             desc = "Detach a CLI Session",
         },
         {
-            "<leader>at",
+            "<leader>st",
             function() require("sidekick.cli").send({ msg = "{this}" }) end,
             mode = { "x", "n" },
             desc = "Send This",
         },
         {
-            "<leader>af",
+            "<leader>sf",
             function() require("sidekick.cli").send({ msg = "{file}" }) end,
             desc = "Send File",
         },
         {
-            "<leader>av",
+            "<leader>sv",
             function() require("sidekick.cli").send({ msg = "{selection}" }) end,
             mode = { "x" },
             desc = "Send Visual Selection",
         },
         {
-            "<leader>ap",
+            "<leader>sp",
             function() require("sidekick.cli").prompt() end,
             mode = { "n", "x" },
             desc = "Sidekick Select Prompt",
         },
         -- Example of a keybinding to open Claude directly
         {
-            "<leader>ac",
+            "<leader>sc",
             function() require("sidekick.cli").toggle({ name = "claude", focus = true }) end,
             desc = "Sidekick Toggle Claude",
         },
