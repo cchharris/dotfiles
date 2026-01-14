7f44813a1e4e891b4f610e3a7f610d43bf1682a3
Christopher Harris <cchharris@outlook.com>
2026-01-14 15:49:57 -0800

    Auto-enable treesitter highlighting

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -43,8 +43,13 @@ return {
             "yaml",
             "zsh",
         });
-        -- sync_install = false,
-        -- highlight = { enable = true },
-        -- indent = { enable = true },
+        vim.api.nvim_create_autocmd("FileType", {
+            group = vim.api.nvim_create_augroup("EnableTreesitterHighlighting", { clear = true }),
+            desc = "Try to enable tree-sitter syntax highlighting",
+            pattern = '*',
+            callback = function()
+                pcall(function() vim.treesitter.start() end)
+            end
+        })
     end
 }
