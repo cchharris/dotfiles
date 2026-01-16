6ace654f9384770389253fa08ce4730e965d8333
Christopher Harris <cchharris@outlook.com>
2026-01-16 09:38:38 -0800

    Hrrrm. This breaks functionality that works as expected elsewhere

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     branch = "main",
-    event = "VeryLazy",
+    lazy = false,
     build = ":TSUpdate",
     config = function()
         local ts = require("nvim-treesitter")
@@ -43,13 +43,13 @@ return {
             "yaml",
             "zsh",
         });
-        -- vim.api.nvim_create_autocmd("FileType", {
-        --     group = vim.api.nvim_create_augroup("EnableTreesitterHighlighting", { clear = true }),
-        --     desc = "Try to enable tree-sitter syntax highlighting",
-        --     pattern = '*',
-        --     callback = function()
-        --         pcall(function() vim.treesitter.start() end)
-        --     end
-        -- })
+        vim.api.nvim_create_autocmd("FileType", {
+            group = vim.api.nvim_create_augroup("EnableTreesitterHighlighting", { clear = true }),
+            desc = "Try to enable tree-sitter syntax highlighting",
+            pattern = '*',
+            callback = function()
+                pcall(function() vim.treesitter.start() end)
+            end
+        })
     end
 }
