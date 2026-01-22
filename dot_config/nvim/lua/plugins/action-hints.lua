bb99fa7a41b071a3d7a10c92fdfe0f2c14fb4b4f
Christopher Harris <cchharris@outlook.com>
2026-01-22 11:12:47 -0800

    Add defs/impl/refs text

--- dot_config/nvim/lua/plugins/action-hints.lua
+++ dot_config/nvim/lua/plugins/action-hints.lua
@@ -1,4 +1,13 @@
 return {
     "roobert/action-hints.nvim",
-    opts = {},
+    config = function()
+        require('action-hints').setup({
+            template = {
+                definition = { text = " ⊛", color = "#add8e6" },
+                references = { text = " ↱%s", color = "#ff6666" },
+            },
+            use_virtual_text = true,
+        }
+        )
+    end
 }
